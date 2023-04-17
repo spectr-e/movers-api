eb047da657703dd8ae31c1531a2d561f5fb18a87
ngachadavid <davidngacha01@gmail.com>
2023-04-17 18:23:56 +0300

    fix: add box model

--- app/models/box.rb
+++ app/models/box.rb
@@ -1,2 +1,19 @@
 class Box < ApplicationRecord
+    validates :range, presence: true
+    validates :cost_to_move_boxes, presence: true, numericality: { only_integer: true }
 end
+
+
+# class Box < ApplicationRecord
+#     BOX_RANGES = {
+#       "1-10" => 5000,
+#       "11-20" => 7000,
+#       "20-30" => 12000,
+#       "30-50" => 20000,
+#       "50+" => 30000
+#     }.freeze
+
+#     def cost_to_move
+#       BOX_RANGES[range]
+#     end
+# end
