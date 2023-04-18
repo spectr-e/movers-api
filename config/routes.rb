32a920e7019a664fec3252a66f1e07d68137bd6b
Charlotte283 <charlotte@charl-s>
2023-04-18 19:41:14 +0300

    added auth for user

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :users
   root to: redirect('/healthcheck', status: 302)
   get "/healthcheck", to: proc { [200, {}, ["success"]] }
