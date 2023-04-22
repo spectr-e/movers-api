9bfe5975e6856afa6def3dd33a25a600154339b3
jos-the-top-dev <devs.josia@gmail.com>
2023-04-22 20:29:31 +0300

    fix: remove unnecessary code

--- app/models/apartment.rb
+++ app/models/apartment.rb
@@ -1,21 +1,5 @@
 class Apartment < ApplicationRecord
     has_many :apartment_inventories
     has_many :inventories, through: :apartment_inventories
-
     validates :size, presence: true
-
-    # By using #freeze, I'm able to create a constant that's actually constant. This time, when I attempt to modify the string, I get a RuntimeError.
-    LABOUR_COST = {
-        "bedsitter" => 10000,
-        "1 bedroom" => 15000,
-        "studio" => 15000,
-        "2 bedroom" => 18000
-    }.freeze
-
-    LABOUR = {
-        "bedsitter" => 1,
-        "1 bedroom" => 2,
-        "studio" => 2,
-        "2 bedroom" => 4
-    }.freeze
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,38 @@
 Box.create(range: "1-5", cost_to_move_boxes: 5000)
 Box.create(range: "6-10", cost_to_move_boxes: 7000)
 Box.create(range: "11-15", cost_to_move_boxes: 9000)
-Box.create(range: "16-20", cost_to_move_boxes: 12000)
-Box.create(range: "21-30", cost_to_move_boxes: 16000)
-Box.create(range: "31-40", cost_to_move_boxes: 20000)
+Box.create(range: "16-20", cost_to_move_boxes: 12_000)
+Box.create(range: "21-30", cost_to_move_boxes: 16_000)
+Box.create(range: "31-40", cost_to_move_boxes: 20_000)
 Box.create(range: "41-50", cost_to_move_boxes: 2500)
-Box.create(range: "50+", cost_to_move_boxes: 30000)
-
+Box.create(range: "50+", cost_to_move_boxes: 30_000)
 
 # Create apartment sizes with labour & cost
-Apartment.create(size: 'bedsitter', description: 'A small living space that includes boxed and bag items and a bed', labour: '1', labour_cost: 10000)
-Apartment.create(size: '1 bedroom', description: '1 bedroom property that includes boxed and bag items and multiple items of furniture', labour: '2', labour_cost: 15000)
-Apartment.create(size: 'studio', description: 'A move with boxes, bags and limited furniture. For example, a bed, wardrobe and kitchenette ', labour: '2', labour_cost: 15000)
-Apartment.create(size: '2 bedroom', description: '2 bedroom property that includes boxed and bag items and multiple items of furniture', labour: '4', labour_cost: 18000)
+Apartment.create(
+  size: "bedsitter",
+  description:
+    "A small living space that includes boxed and bag items and a bed",
+  labour: "1",
+  labour_cost: 10_000,
+)
+Apartment.create(
+  size: "1 bedroom",
+  description:
+    "1 bedroom property that includes boxed and bag items and multiple items of furniture",
+  labour: "2",
+  labour_cost: 15_000,
+)
+Apartment.create(
+  size: "studio",
+  description:
+    "A move with boxes, bags and limited furniture. For example, a bed, wardrobe and kitchenette ",
+  labour: "2",
+  labour_cost: 15_000,
+)
+Apartment.create(
+  size: "2 bedroom",
+  description:
+    "2 bedroom property that includes boxed and bag items and multiple items of furniture",
+  labour: "4",
+  labour_cost: 18_000,
+)
