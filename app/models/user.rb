dc201bfc62ef0d8b149828632354bb23b3efdc85
jos-the-top-dev <devs.josia@gmail.com>
2023-04-22 18:02:03 +0300

    fix: remove users resource to start a fresh

--- app/models/user.rb
+++
@@ -1,9 +0,0 @@
-class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  has_secure_password
-
-  def generate_jwt
-    JWT.encode({ user_id: id }, 'secret_key', 'HS256')
-  end
-end
