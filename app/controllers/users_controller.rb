f11b1e0c5edc7b9b2607047250453e3fcaeed453
Charlotte283 <charlotte@charl-s>
2023-04-22 13:20:25 +0300

    fix : update user methods in routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,14 @@
 class UsersController < ApplicationController
   before_action :set_user, only: %i[ show edit update destroy ]
+  before_action :set_user, only: %i[ show edit update destroy ]
 
+  # GET /users or /users.json
   # GET /users or /users.json
   def index
     @users = User.all
   end
 
+  # GET /users/1 or /users/1.json
   # GET /users/1 or /users/1.json
   def show
   end
@@ -15,6 +18,16 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  # GET /users/new
+  def new
+    @user = User.new
+  end
+
+  # GET /users/1/edit
+  def edit
+  end
+
+  # POST /users or /users.json
   # GET /users/1/edit
   def edit
   end
@@ -31,11 +44,31 @@ class UsersController < ApplicationController
         format.html { render :new, status: :unprocessable_entity }
         format.json { render json: @user.errors, status: :unprocessable_entity }
       end
+    @user = User.new(user_params)
+
+    respond_to do |format|
+      if @user.save
+        format.html { redirect_to user_url(@user), notice: "User was successfully created." }
+        format.json { render :show, status: :created, location: @user }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
     end
+  end
   end
 
   # PATCH/PUT /users/1 or /users/1.json
+  # PATCH/PUT /users/1 or /users/1.json
   def update
+    respond_to do |format|
+      if @user.update(user_params)
+        format.html { redirect_to user_url(@user), notice: "User was successfully updated." }
+        format.json { render :show, status: :ok, location: @user }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
     respond_to do |format|
       if @user.update(user_params)
         format.html { redirect_to user_url(@user), notice: "User was successfully updated." }
@@ -48,9 +81,14 @@ class UsersController < ApplicationController
   end
 
   # DELETE /users/1 or /users/1.json
+  # DELETE /users/1 or /users/1.json
   def destroy
     @user.destroy
+    @user.destroy
 
+    respond_to do |format|
+      format.html { redirect_to users_url, notice: "User was successfully destroyed." }
+      format.json { head :no_content }
     respond_to do |format|
       format.html { redirect_to users_url, notice: "User was successfully destroyed." }
       format.json { head :no_content }
@@ -62,9 +100,17 @@ class UsersController < ApplicationController
     def set_user
       @user = User.find(params[:id])
     end
+    # Use callbacks to share common setup or constraints between actions.
+    def set_user
+      @user = User.find(params[:id])
+    end
 
     # Only allow a list of trusted parameters through.
     def user_params
       params.require(:user).permit(:name, :primary_email, :secondary_email, :primary_phone_number, :secondary_phone_number, :image, :password_digest)
+    # Only allow a list of trusted parameters through.
+    def user_params
+      params.require(:user).permit(:name, :primary_email, :secondary_email, :primary_phone_number, :secondary_phone_number, :image, :password_digest)
     end
+end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  resources :users
+  
+  resources :users ,only: [:index , :show ,:create ,:update ,:destroy]
+  resources :boxes
+  resources :apartment_inventories
+  resources :movers
+  resources :apartment_sizes
+  resources :inventories
   root to: redirect('/healthcheck', status: 302)
   get "/healthcheck", to: proc { [200, {}, ["success"]] }
 end
