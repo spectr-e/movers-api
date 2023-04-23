3fb6ed6186caf64d1f8615a4cfecd8c85d57e05b
jos-the-top-dev <devs.josia@gmail.com>
2023-04-23 13:18:34 +0300

    fix: allow create user method to render jwt token

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,17 +5,41 @@ class UsersController < ApplicationController
     render json: User.all, status: :ok
   end
 
+  # SignUp User
   def create
     user = User.create!(create_user_params)
-    render json: user, status: :created
+    token = encode_token({ user_id: user.id })
+    render json: {
+             user: UserSerializer.new(user),
+             jwt: token,
+           },
+           status: :created
+  end
+
+  # User Profile
+  def show
+    render json: current_user, status: :accepted
   end
 
   private
+
   def create_user_params
-    params.permit(:name, :email, :password, :password_confirmation, :phone, :add_email, :add_phone, :image)
+    params.permit(
+      :name,
+      :email,
+      :password,
+      :password_confirmation,
+      :phone,
+      :add_email,
+      :add_phone,
+      :image,
+    )
   end
 
   def record_invalid(invalid)
-    render json: { error: "Failed to create user, validation error!" }, status: :unprocessable_entity
+    render json: {
+             error: "Failed to create user, validation error!",
+           },
+           status: :unprocessable_entity
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :users
+  resources :users, only: %i[index create update destory]
   resources :boxes
   resources :apartment_inventories
   resources :movers
@@ -8,9 +8,9 @@ Rails.application.routes.draw do
   resources :ratings, only: %i[index create show update destroy]
   resources :bookings, only: %i[index create show update destroy]
 
-  post '/login', to: 'sessions#create'
-  
+  post "/login", to: "sessions#create"
+  get "/profile", to: "user#show"
+
   root to: redirect("/healthcheck", status: 302)
   get "/healthcheck", to: proc { [200, {}, ["success"]] }
-
 end
