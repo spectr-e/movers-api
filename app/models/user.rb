0c0643e249fe3da8bc5d30a14e7ef7fc912f173b
jos-the-top-dev <devs.josia@gmail.com>
2023-04-23 13:28:52 +0300

    fix: add validations to user model, make effective migrations

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,13 @@ class User < ApplicationRecord
   has_many :apartment_inventories, through: :apartments
   has_many :inventories, through: :apartment_inventories
 
+  validates :email, presence: true, uniqueness: true
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :phone, format: { with: /\A\+254\d{9}\z/, message: "must be in the format +254XXXXXXXXX" }
+  validates :add_phone, format: { with: /\A\+254\d{9}\z/, message: "must be in the format +254XXXXXXXXX" }
+  validates :password,
+            length: { minimum: 6 },
+            if: -> { new_record? || !password.nil? }
+
   has_secure_password
 end
