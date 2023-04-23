c734a409109d88726a40a74071756b489b9e8d5c
jos-the-top-dev <devs.josia@gmail.com>
2023-04-23 12:56:58 +0300

    feat: add login authentication!

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  skip_before_action :authorize, only: [:create]
   rescue_from ActiveRecord::RecordInvalid, with: :record_invalid
   def index
     render json: User.all, status: :ok
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
   resources :inventories
   resources :ratings, only: %i[index create show update destroy]
   resources :bookings, only: %i[index create show update destroy]
+
+  post '/login', to: 'sessions#create'
   
   root to: redirect("/healthcheck", status: 302)
   get "/healthcheck", to: proc { [200, {}, ["success"]] }
