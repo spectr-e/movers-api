b66589cef89fcbf08eed976f64ea48a3c2ba122c
jos-the-top-dev <devs.josia@gmail.com>
2023-04-23 13:49:51 +0300

    fix: remove phone validation errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,9 @@ class User < ApplicationRecord
   has_many :apartment_inventories, through: :apartments
   has_many :inventories, through: :apartment_inventories
 
+  validates :name, presence: true
   validates :email, presence: true, uniqueness: true
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
-  validates :phone, format: { with: /\A\+254\d{9}\z/, message: "must be in the format +254XXXXXXXXX" }
-  validates :add_phone, format: { with: /\A\+254\d{9}\z/, message: "must be in the format +254XXXXXXXXX" }
   validates :password,
             length: { minimum: 6 },
             if: -> { new_record? || !password.nil? }
