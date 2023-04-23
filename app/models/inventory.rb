8c5dc6c111ecabc9c666b4f081ab9cb47bac0f5e
ngachadavid <davidngacha01@gmail.com>
2023-04-23 18:05:17 +0300

    fix: create a new branch to remodel inventory

--- app/models/inventory.rb
+++ app/models/inventory.rb
@@ -3,5 +3,12 @@ class Inventory < ApplicationRecord
     has_many :apartment_inventories
 
     #validations
-    validates :name, presence: true, uniqueness: true
+    validates :name, presence: true
+    #search by query parameter and use ILIKE operator for case-insensitive search
+    def self.search(query)
+        where ('name ILIKE ?', '%#{query}%')
+    else
+        all
+    end
+   end
 end
