f2efb29e0e81ab21b50a42bb3b2eb62211f2ec3b
ngachadavid <davidngacha01@gmail.com>
2023-04-24 18:27:57 +0300

    fix:comment out scope feature in model until i test it on react!

--- app/models/box.rb
+++ app/models/box.rb
@@ -4,6 +4,6 @@ class Box < ApplicationRecord
     validates :cost_to_move_boxes, presence: true, numericality: { only_integer: true }
 
     #scope ordering boxes by numerical vlue of the first digits in range field in ascending order
-    scope :ordered_by_range, -> { order(Arel.sql("cast(substring(range, '^[0-9]+') as integer)")) }
+    # scope :ordered_by_range, -> { order(Arel.sql("cast(substring(range, '^[0-9]+') as integer)")) }
 
 end
