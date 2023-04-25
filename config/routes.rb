3329dceeae6eaea0cd6ec0b9ad3012386cd1daed
Charlotte283 <charlotte@charl-s>
2023-04-25 08:16:43 +0300

    fix: correct syntax for mover routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :movers , only:[:index, :create ,:edit, :show]
+  resources :movers , only:[:index, :create ,:edit, :show, :update ,:destroy]
   root to: redirect('/healthcheck', status: 302)
   get "/healthcheck", to: proc { [200, {}, ["success"]] }
 end
