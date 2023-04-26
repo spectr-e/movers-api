6bb89b7a1055b4e1b1a571919ddc52736346770d
ngachadavid <davidngacha01@gmail.com>
2023-04-26 18:02:02 +0300

    fix:add apartment controller test

--- test/controllers/apartment_controller_test.rb
+++ test/controllers/apartment_controller_test.rb
@@ -1,7 +1,38 @@
-require "test_helper"
+require 'test_helper'
 
-class ApartmentSizesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+class ApartmentsControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @apartment = apartments(:one) # assuming there is a fixture file for apartments
+  end
+
+  test "should get index" do
+    get apartments_url
+    assert_response :success
+  end
+
+  test "should show apartment" do
+    get apartment_url(@apartment)
+    assert_response :success
+  end
+
+  test "should create apartment" do
+    assert_difference('Apartment.count') do
+      post apartments_url, params: { apartment: { size: @apartment.size, description: @apartment.description, labour: @apartment.labour, labour_cost: @apartment.labour_cost } }
+    end
+
+    assert_response :created
+  end
+
+  test "should update apartment" do
+    patch apartment_url(@apartment), params: { apartment: { size: @apartment.size, description: @apartment.description, labour: @apartment.labour, labour_cost: @apartment.labour_cost } }
+    assert_response :success
+  end
+
+  test "should destroy apartment" do
+    assert_difference('Apartment.count', -1) do
+      delete apartment_url(@apartment)
+    end
+
+    assert_response :no_content
+  end
 end
